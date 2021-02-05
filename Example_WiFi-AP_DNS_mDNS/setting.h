10962b657f9a84002cd24f75c5d633b4268132f1
CcydtN <51289140+CcydtN@users.noreply.github.com>
2021-02-05 14:26:24 +0800

    Add example for wifi-ap, DNS & mDNS

---
+++ Example_WiFi-AP_DNS_mDNS/setting.h
@@ -0,0 +1,7 @@
+// Replace with your network credentials
+const char* ssid = "YourWiFi";
+const char* password = "YourPassword";
+
+// The Access point setting
+const char* _AP_SSID = "ESP32";
+const char* _AP_PW = "qwertyui";
