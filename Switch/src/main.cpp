482ef22dbed01a622cf9b82fd722353e0265bd5b
CcydtN <51289140+CcydtN@users.noreply.github.com>
2021-03-13 02:33:05 +0800

    The remove function fail, new method implemented

--- Switch/src/main.cpp
+++ Switch/src/main.cpp
@@ -73,15 +73,16 @@ void addClient(String id, String identifier = "", bool status = false){
 }
 
 void test(){
+  int id = Cron.getTriggeredCronId();
   if(getLocalTime(&timeinfo)){
-    Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
+    Serial.print("Cron Task");
+    Serial.print(id);
+    Serial.println(&timeinfo, "\t%A, %B %d %Y %H:%M:%S");
   }
 }
 
 void removeAllCron(){
-  for(uint8_t id=0;id<Cron.count();++id){
-    Cron.free(id);
-  }
+  Cron = CronClass();
 }
 
 /*
@@ -190,12 +191,10 @@ void setup() {
     String timing = str.substring(0,str.lastIndexOf(' ')-1);
 //////////
     int cronid = Cron.create((char *)"*/20 * * * * *", test, false);
-    Serial.println(cronid);
     cronid = Cron.create((char *)"*/30 * * * * *", test, false);
-    Serial.println(cronid);
     removeAllCron();
     cronid = Cron.create((char *)"*/20 * * * * *", test, false);
-    Serial.println(cronid);
+
 
   }else{
     //Access point
